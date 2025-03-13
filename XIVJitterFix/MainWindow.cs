f9c2948ed496200e9829661ee668893c659e6cc6
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-03-13 14:16:31 +0800

    API 11 CN Fix

--- XIVJitterFix/MainWindow.cs
+++ XIVJitterFix/MainWindow.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
@@ -8,87 +10,84 @@ namespace XIVJitterFix;
 public class MainWindow : Window
 {
     private readonly IDalamudPluginInterface dalamudPluginInterface;
-    private readonly Config pluginConfig;
+    private readonly Config                  pluginConfig;
 
-    public MainWindow(Config config, IDalamudPluginInterface dalamudPluginInterface) : base("XIVJitterFix Info###XivJitterFix")
+    public MainWindow(Config config, IDalamudPluginInterface dalamudPluginInterface) : base("XIVJitterFix 配置###XivJitterFix")
     {
-        Size = new System.Numerics.Vector2(500, 400);
+        Size                        = new Vector2(500, 400);
         this.dalamudPluginInterface = dalamudPluginInterface;
-        this.pluginConfig = config;
-    }
-
-    private void WrapText(string text)
-    {
-        ImGui.PushTextWrapPos(0);
-        ImGui.TextUnformatted(text);
-        ImGui.PopTextWrapPos();
+        pluginConfig                = config;
     }
 
     public override void Draw()
     {
-        WrapText("This plugin is active automatically. To turn off the functionality, just disable the plugin.");
+        ImGui.TextWrapped("启用插件代表启用相关功能, 如需关闭, 直接禁用本插件即可");
         ImGui.Separator();
-        WrapText("Warning: Do not use this plugin if you don't use DLAA, DLSS or TSCMAA+jitter.");
+        
+        ImGui.TextWrapped("提示: 如果你并未在画面设置里使用 DLAA、DLSS 或 TSCMAA+ , 请勿启用本插件");
         ImGui.Separator();
-        var help = ImRaii.TreeNode("What is jitter and why do I want it?");
-        if (help)
+
+        using (var help = ImRaii.TreeNode("什么是画面抖动？为什么需要它？"))
         {
-
-            WrapText("Jittering the camera is an essential part of temporal antialiasing solutions like the aforementioned ones. " +
-                "The micromovements introduced by jitter resolve aliased and more static edges " +
-                "and especially help with alpha transparencies like foliage. Whenever possible, jitter should be used.");
-            WrapText("This normally happens automatically and so it does in this game, but for some reason SE decided to not jitter during cutscenes, GPose and NPC dialogue.");
-            WrapText("You may have noticed that the antialiasing gets worse once you do one of the mentioned tasks. This is because jitter gets disabled.");
-            WrapText("From my testing I have found no reason why jitter should ever be disabled and it improves the image quality especially in those situations.");
-        }
-        help.Dispose();
-
-        ImGui.Separator();
-        var expertConfig = ImRaii.TreeNode("Expert Config");
-        if (expertConfig)
-        {
-            float jitterMultiplier = pluginConfig.JitterMultiplier;
-            ImGui.SetNextItemWidth(150);
-            bool configDirty = false;
-            if (ImGui.SliderFloat("Jitter Multiplier", ref jitterMultiplier, 0.1f, 3.0f))
+            if (help)
             {
-                pluginConfig.JitterMultiplier = jitterMultiplier;
-                configDirty = true;
-            }
-            ImGui.SameLine();
-            if (ImGui.Button("Reset"))
-            {
-                pluginConfig.JitterMultiplier = 0.6f;
-                configDirty = true;
-            }
-            WrapText("This multiplier steers how much jittering is done through the game. 0.6 is default. Play around and see what works best for you. " +
-                "Sane values are probably somewhere between 0.5 and 1.5. If you need more precise values use Ctrl+Click into the slider. " +
-                "Jitter is more noticeable with TSCMAA+jitter than DLAA.");
-
-            bool setDownscaleBuffers = pluginConfig.SetDownscaleBuffers;
-            if (ImGui.Checkbox("Override Downscaling Settings", ref setDownscaleBuffers))
-            {
-                pluginConfig.SetDownscaleBuffers = setDownscaleBuffers;
-                configDirty = true;
-            }
-            using (ImRaii.Disabled(!setDownscaleBuffers))
-            {
-                using var _ = ImRaii.PushIndent(10f);
-
-                bool ignoreDownscaling = pluginConfig.DownscaleBuffers == 0;
-                if (ImGui.Checkbox("Ignore Downscaling", ref ignoreDownscaling))
-                {
-                    pluginConfig.DownscaleBuffers = (byte)(ignoreDownscaling ? 0 : 1);
-                    configDirty = true;
-                }
-                WrapText("This appears to fix the bloom and depth of field buffers when running any downscaling. Turn this on if you use DLAA. Note: this will also force DLAA on always as no downscaling will be performed anymore.");
-                if (configDirty)
-                {
-                    dalamudPluginInterface.SavePluginConfig(pluginConfig);
-                }
+                ImGui.TextWrapped("相机抖动是时间性抗锯齿技术（如前述方案）的重要组成部分," +
+                                  "通过细微位移可有效消除锯齿边缘和静态边缘的走样,"     +
+                                  "尤其能改善植被等半透明物体的显示效果。建议在条件允许时始终启用抖动");
+                ImGui.TextWrapped("游戏通常会自动启用抖动，FF14 也不例外。但 SE 出于某些原因，在过场动画、GPose 和 NPC 对话 期间禁用了该功能。");
+                ImGui.TextWrapped("你可能已注意到在上述场景中游戏自带的抗锯齿效果明显下降, 这正是因为抖动功能被关闭所致");
+                ImGui.TextWrapped("经测试, 我们认为没有任何理由需要禁用抖动功能。启用后能显著提升这些场景下的画面质量");
             }
         }
 
-        expertConfig.Dispose();
+        ImGui.Separator();
+        using (var expertConfig = ImRaii.TreeNode("高级设置"))
+        {
+            if (expertConfig)
+            {
+                var jitterMultiplier = pluginConfig.JitterMultiplier;
+                var configDirty = false;
+                ImGui.SetNextItemWidth(150f * ImGuiHelpers.GlobalScale);
+                if (ImGui.SliderFloat("抖动强度倍数", ref jitterMultiplier, 0.1f, 3.0f))
+                {
+                    pluginConfig.JitterMultiplier = jitterMultiplier;
+                    configDirty                   = true;
+                }
+
+                ImGui.SameLine();
+                if (ImGui.Button("重置"))
+                {
+                    pluginConfig.JitterMultiplier = 0.6f;
+                    configDirty                   = true;
+                }
+
+                ImGui.TextWrapped(
+                    "此参数控制画面抖动强度, 默认值为0.6, 建议在0.5-1.5范围内调试, Ctrl+单击滑块 可输入精确值。" +
+                    "注意：使用 TSCMAA+ 抖动技术时，抖动效果比 DLAA 更为明显。");
+
+                var setDownscaleBuffers = pluginConfig.SetDownscaleBuffers;
+                if (ImGui.Checkbox("覆盖降采样设置", ref setDownscaleBuffers))
+                {
+                    pluginConfig.SetDownscaleBuffers = setDownscaleBuffers;
+                    configDirty                      = true;
+                }
+
+                using (ImRaii.Disabled(!setDownscaleBuffers))
+                {
+                    using var _ = ImRaii.PushIndent(10f);
+
+                    var ignoreDownscaling = pluginConfig.DownscaleBuffers == 0;
+                    if (ImGui.Checkbox("忽略降采样", ref ignoreDownscaling))
+                    {
+                        pluginConfig.DownscaleBuffers = (byte)(ignoreDownscaling ? 0 : 1);
+                        configDirty                   = true;
+                    }
+
+                    ImGui.TextWrapped("此选项可在使用降采样技术时修复光晕和景深缓冲区问题。启用 DLAA 时建议开启，注意：这将强制永久启用 DLAA 且不再进行任何降采样处理");
+                    if (configDirty)
+                        dalamudPluginInterface.SavePluginConfig(pluginConfig);
+                }
+            }
+        }
     }
 }
--- XIVJitterFix/Plugin.cs
+++ XIVJitterFix/Plugin.cs
@@ -1,12 +1,12 @@
-using Dalamud.Plugin;
-using Dalamud.Plugin.Services;
-using Dalamud.Game;
-using System.Runtime.InteropServices;
-using Dalamud.Interface.Windowing;
-using Dalamud.Game.Command;
 using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
+using Dalamud.Game;
+using Dalamud.Game.Command;
 using Dalamud.Interface.ImGuiNotification;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
 
 namespace XIVJitterFix;
 
@@ -14,25 +14,27 @@ public sealed class Plugin : IDalamudPlugin
 {
     private readonly nint hookAddr;
 
-    private readonly IPluginLog logger;
-    private readonly IFramework framework;
-    private readonly WindowSystem windowSystem;
+    private readonly IPluginLog              logger;
+    private readonly IFramework              framework;
+    private readonly WindowSystem            windowSystem;
     private readonly IDalamudPluginInterface dalamudPluginInterface;
-    private readonly ICommandManager commandManager;
-    private readonly MainWindow mainWindow;
-    private readonly Config pluginConfig;
-    private readonly INotificationManager notificationManager;
+    private readonly ICommandManager         commandManager;
+    private readonly MainWindow              mainWindow;
+    private readonly Config                  pluginConfig;
+    private readonly INotificationManager    notificationManager;
 
-    public unsafe Plugin(IPluginLog logger, ISigScanner sigScanner,
-        IFramework framework, IDalamudPluginInterface dalamudPluginInterface, ICommandManager commandManager, INotificationManager notificationManager)
+    public Plugin(
+        IPluginLog           logger,    ISigScanner             sigScanner,
+        IFramework           framework, IDalamudPluginInterface dalamudPluginInterface, ICommandManager commandManager,
+        INotificationManager notificationManager)
     {
-        this.logger = logger;
-        this.framework = framework;
+        this.logger                 = logger;
+        this.framework              = framework;
         this.dalamudPluginInterface = dalamudPluginInterface;
-        this.commandManager = commandManager;
-        this.notificationManager = notificationManager;
-        windowSystem = new("XIVJitterFix");
-        pluginConfig = dalamudPluginInterface.GetPluginConfig() as Config ?? new();
+        this.commandManager         = commandManager;
+        this.notificationManager    = notificationManager;
+        windowSystem                = new("XIVJitterFix");
+        pluginConfig                = dalamudPluginInterface.GetPluginConfig() as Config ?? new();
 
         if (pluginConfig.Version == 0)
         {
@@ -42,6 +44,7 @@ public sealed class Plugin : IDalamudPlugin
                 logger.Info("DownscaleBuffers was set to 0, setting SetDownscaleBuffers to true");
                 pluginConfig.SetDownscaleBuffers = true;
             }
+
             pluginConfig.Version = 1;
             dalamudPluginInterface.SavePluginConfig(pluginConfig);
         }
@@ -49,17 +52,18 @@ public sealed class Plugin : IDalamudPlugin
         mainWindow = new MainWindow(pluginConfig, dalamudPluginInterface);
         windowSystem.AddWindow(mainWindow);
 
-        commandManager.AddHandler("/jitterfix", new CommandInfo(OnCommand) 
-        { 
-            HelpMessage = "Open the XIVJitterFix config window.\n" +
-            "/jitterfix jitter <value> → Sets jitter multiplier to a specific value.", ShowInHelp = true 
+        commandManager.AddHandler("/jitterfix", new CommandInfo(OnCommand)
+        {
+            HelpMessage = "打开插件配置界面\n" +
+                          "/jitterfix jitter <value> → 设置指定的抖动倍率",
+            ShowInHelp = true
         });
 
         hookAddr = sigScanner.GetStaticAddressFromSig("48 89 05 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 8F ?? ?? ?? ??");
 
-        framework.Update += Framework_Update;
+        framework.Update                              += Framework_Update;
         dalamudPluginInterface.UiBuilder.OpenConfigUi += UiBuilder_OpenConfigUi;
-        dalamudPluginInterface.UiBuilder.Draw += UiBuilder_Draw;
+        dalamudPluginInterface.UiBuilder.Draw         += UiBuilder_Draw;
     }
 
 
@@ -67,27 +71,35 @@ public sealed class Plugin : IDalamudPlugin
     {
         var splitArgs = args.ToLowerInvariant().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries); //Setting specific commands?
 
-        if (splitArgs.Length == 0)
+        switch (splitArgs.Length)
         {
-            mainWindow.Toggle();
-        }
-
-        if(splitArgs.Length == 2)
-        {
-            if (splitArgs[0] == "jitter")
+            case 0:
+                mainWindow.Toggle();
+                break;
+            case 2:
             {
-                float jittermulti;
-                if (float.TryParse(splitArgs[1].Replace(",", "."), CultureInfo.InvariantCulture.NumberFormat, out jittermulti))
+                if (splitArgs[0] == "jitter")
                 {
-                    pluginConfig.JitterMultiplier = jittermulti;
-                    dalamudPluginInterface.SavePluginConfig(pluginConfig);
-                    notificationManager.AddNotification(new Notification() { Content = $"Jitter set to {jittermulti}", Title = "Jitter value change", Type = NotificationType.Success });
+                    if (float.TryParse(splitArgs[1].Replace(",", "."), CultureInfo.InvariantCulture.NumberFormat, out var jittermulti))
+                    {
+                        pluginConfig.JitterMultiplier = jittermulti;
+                        dalamudPluginInterface.SavePluginConfig(pluginConfig);
+                        notificationManager.AddNotification(new Notification
+                        {
+                            Content = $"抖动值已被设置为 {jittermulti}", Title = "抖动值变更", Type = NotificationType.Success
+                        });
+                    }
+                    else
+                    {
+                        logger.Warning("Provided value {0} is not a valid float number", splitArgs[1]);
+                        notificationManager.AddNotification(new Notification
+                        {
+                            Content = "无法将抖动值设置为输入参数", Title = "抖动值变更", Type = NotificationType.Error
+                        });
+                    }
                 }
-                else
-                {
-                    logger.Warning("Provided value {0} is not a valid float number", splitArgs[1]);
-                    notificationManager.AddNotification(new Notification() { Content = "Failed to set jitter to provided value", Title = "Jitter value change", Type = NotificationType.Error });
-                }
+
+                break;
             }
         }
     }
@@ -102,13 +114,13 @@ public sealed class Plugin : IDalamudPlugin
         mainWindow.Toggle();
     }
 
-    private unsafe void Framework_Update(IFramework framework)
+    private unsafe void Framework_Update(IFramework _)
     {
         if (hookAddr == nint.Zero) return;
         var addr1 = *(nint*)hookAddr;
         if (addr1 == nint.Zero) return;
 
-        ProbablySomeGraphicsConfig* config = (ProbablySomeGraphicsConfig*)addr1;
+        var config = (ProbablySomeGraphicsConfig*)addr1;
 
         var prevValue1 = config->NpcGposeJitter;
         var prevValue2 = config->CutsceneJitter;
@@ -117,7 +129,8 @@ public sealed class Plugin : IDalamudPlugin
         {
             config->NpcGposeJitter = 1;
             config->CutsceneJitter = 1;
-            logger.Verbose("Detected change NpcGpose {0} / Cutscene {1} -> NpcGpose {2} / Cutscene {3}", prevValue1, prevValue2, config->NpcGposeJitter, config->CutsceneJitter);
+            logger.Verbose("Detected change NpcGpose {0} / Cutscene {1} -> NpcGpose {2} / Cutscene {3}", prevValue1, prevValue2,
+                           config->NpcGposeJitter, config->CutsceneJitter);
         }
 
         if (pluginConfig.JitterMultiplier != config->JitterMultiplier)
@@ -141,36 +154,45 @@ public sealed class Plugin : IDalamudPlugin
         windowSystem.RemoveAllWindows();
 
         dalamudPluginInterface.UiBuilder.OpenConfigUi -= UiBuilder_OpenConfigUi;
-        dalamudPluginInterface.UiBuilder.Draw -= UiBuilder_Draw;
+        dalamudPluginInterface.UiBuilder.Draw         -= UiBuilder_Draw;
 
         framework.Update -= Framework_Update;
     }
 
     [StructLayout(LayoutKind.Explicit)]
-    public partial struct ProbablySomeGraphicsConfig
+    public struct ProbablySomeGraphicsConfig
     {
-        [FieldOffset(0x19)] public byte NpcGposeJitter;
-        [FieldOffset(0x1a)] public byte CutsceneJitter;
+        [FieldOffset(0x19)]
+        public byte NpcGposeJitter;
+
+        [FieldOffset(0x1a)]
+        public byte CutsceneJitter;
 
         /// <summary>
-        /// 0 = nothing, 1 = fxaa, 2 = tscmaa+jitter, 3 = tscmaa
+        ///     0 = nothing, 1 = fxaa, 2 = tscmaa+jitter, 3 = tscmaa
         /// </summary>
-        [FieldOffset(0x2c)] public byte AntiAliasingMode;
+        [FieldOffset(0x2c)]
+        public byte AntiAliasingMode;
 
         /// <summary>
-        /// 0 = off, 1 = on
+        ///     0 = off, 1 = on
         /// </summary>
-        [FieldOffset(0x44)] public byte DynamicResolution;
+        [FieldOffset(0x44)]
+        public byte DynamicResolution;
 
         /// <summary>
-        /// seems like it affects dof/bloom shaders when running dlss or dynamic res
+        ///     seems like it affects dof/bloom shaders when running dlss or dynamic res
         /// </summary>
-        [FieldOffset(0x45)] public byte DownscaleBuffers;
+        [FieldOffset(0x45)]
+        public byte DownscaleBuffers;
 
         /// <summary>
-        /// FSR = 1, DLSS = 2
+        ///     FSR = 1, DLSS = 2
         /// </summary>
-        [FieldOffset(0x54)] public byte DlssFsrSwitch;
-        [FieldOffset(0x64)] public float JitterMultiplier;
+        [FieldOffset(0x54)]
+        public byte DlssFsrSwitch;
+
+        [FieldOffset(0x64)]
+        public float JitterMultiplier;
     }
 }
